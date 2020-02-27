3fc0654319b0f69e50c93819361477a28bbecc3f
Shuanger Li <shuangerli@Shuangers-MBP.fios-router.home>
2020-02-27 16:54:09 -0500

    added pras score python script

---
+++ src/PRASScore.py
@@ -0,0 +1,9 @@
+import sys
+
+if __name__ == "__main__":
+	bedFile = "PureCLIP.crosslink_sites"
+	inFile = open("OutData_500nt_PRAS_option_suggestions.txt", 'r')
+	A = inFile.read().splitlines()[1].split("	")
+	outFile = open("ScoreCommand.txt", 'w')
+	outFile.write("python PRAS_1.0.py -g annotation.gtf -t IDs.txt -m score -s %s -i %s.bed -a %s_%s.assign.txt -w 10 -r %s -d %s" % (A[0], bedFile, bedFile, A[0], A[1], A[2]))
+	outFile.close()
